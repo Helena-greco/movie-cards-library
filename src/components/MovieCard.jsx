5f20a1b8483428d568c3b9582fb6755726b49fe1
Helena <nenafgreco@hotmail.com>
2021-09-01 18:54:51 -0300

    Corrigindo o Linter para os requisitos 1 e 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,34 +1,35 @@
 // implement MovieCard component here
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Rating from './Rating';
 
-class MovieCard extends React.Component {
-  render() {
-    const { movies: { title, subtitle, storyline, rating, imagePath } } = this.props;
-    return (
-      <section>
-        <img src={ imagePath } alt={ `Movie ${title}` } />
-        <div>
-          <h3>{ title }</h3>
-          <h4>{ subtitle }</h4>
-          <p>{ storyline }</p>
-        </div>
-        <div>
-          { rating }
-        </div>
-      </section>
-    );
-  }
-}
+// class MovieCard extends React.Component {
+//   render() {
+//     const { movies: { title, subtitle, storyline, rating, imagePath } } = this.props;
+//     return (
+//       <section>
+//         <img src={ imagePath } alt={ `Movie ${title}` } />
+//         <div>
+//           <h3>{ title }</h3>
+//           <h4>{ subtitle }</h4>
+//           <p>{ storyline }</p>
+//         </div>
+//         <div>
+//           <Rating rating={ rating } />
+//         </div>
+//       </section>
+//     );
+//   }
+// }
 
-MovieCard.propTypes = {
-  movies: PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-    rating: PropTypes.number,
-    imagePath: PropTypes.string,
-  }).isRequired,
-};
+// MovieCard.propTypes = {
+//   movies: PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.number,
+//     imagePath: PropTypes.string,
+//   }).isRequired,
+// };
 
-export default MovieCard;
+// export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,14 @@
 // implement MovieList component here
-import React from 'react';
+// import React from 'react';
+// import MovieCard from './MovieCard';
+// import movies from '../data';
 
-class MovieList extends React.Component {
-  render() {
-    return;
-  }
-}
+// class MovieList extends React.Component {
+//   render() {
+//     return (
+//       movies.map((item) => <MovieCard key={ item.title } movie={ item } />)
+//     );
+//   }
+// }
 
-export default MovieList;
+// export default MovieList;
